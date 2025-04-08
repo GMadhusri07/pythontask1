847ecb432b907eeefa1423702d26b88a229c4ab4
GMadhusri07 <madhusriguntuka07@gmail.com>
2025-04-08 21:33:15 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,30 @@
+# 1.Write a function to calculate the square of a given number and show the result.
+def sample(a):
+    print(a**2)
+sample(5)
+
+
+# 2.Create a function that accepts a name and displays a personalized greeting.
+
+def greet_person(a):
+    print("Hello", a, "welcome to the world of python full stack development")
+greet_person("madhusri guntuka")
+
+
+
+# 3.Write a function that takes two numbers as input and performs addition.
+def sample(a,b):
+    print(a+b)
+sample(6,8)
+
+
+# 4.Write a function to multiply two numbers and display the result.
+def sample(a,b):
+    print(a*b)
+sample(6,8)
+
+
+# 5.Create a function that takes a string as input and prints the length of the string
+def print_string_length(a):
+    print(len(a))
+print_string_length("Madhusri Guntuka")
